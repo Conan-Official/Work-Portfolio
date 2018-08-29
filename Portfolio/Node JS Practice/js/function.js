e34ea56a6bcc18017563a406987f10b0e632aae7
Conan <deltatf1@gmail.com>
2018-08-29 15:15:18 -0400

    updated portfolio

--- Portfolio/Node JS Practice/js/function.js
+++
@@ -1,8 +0,0 @@
-
-
-let add = function() {
-    let num = 1 + 1; 
-    console.log(num);
-}
-
-module.exports = add; 
--- Portfolio/Node JS Practice/js/module.js
+++
@@ -1,8 +0,0 @@
-let url = "http://www.google.com";
-
-
-const log = function(message) {
-    console.log(message);
-}
-
-module.exports.log = log; 
